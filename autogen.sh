e984e1cf44cb02e496828140f12705c46f58050f
Mika Jones <mlljones@icloud.com>
2020-07-16 13:20:06 +0100

    Fix

--- autogen.sh
+++ autogen.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-if [ $# > 1 ];
+if [ $# > 1 && $1 == "--no-mach-o"];
 then
-	if [ $1 == "--no-mach-o" ];
-	then
-		echo "building without machopatchfinder, kernelpatchfinder"
-		cp ./makefiles/include-no-mach-o.make ./include/Makefile.am
-		cp ./makefiles/liboffsetfinder64-no-mach-o.make ./liboffsetfinder64/Makefile.am
-	else 
-		cp ./makefiles/include.make ./include/Makefile.am
-		cp ./makefiles/liboffsetfinder64.make ./liboffsetfinder64/Makefile.am
-	fi
+	echo "building without machopatchfinder, kernelpatchfinder"
+	cp ./makefiles/include-no-mach-o.make ./include/Makefile.am
+	cp ./makefiles/liboffsetfinder64-no-mach-o.make ./liboffsetfinder64/Makefile.am
+else 
+	cp ./makefiles/include.make ./include/Makefile.am
+	cp ./makefiles/liboffsetfinder64.make ./liboffsetfinder64/Makefile.am
 fi
 
 aclocal -I m4
@@ -18,12 +15,9 @@ autoconf
 autoheader
 automake --add-missing
 autoreconf -i
-if [ $# > 1 ];
-then 
-	if [ $1 == "--no-mach-o" ];
-	then 
-		./configure "$0"
-	fi
+if [ $# > 1 && $1 == "--no-mach-o"];
+then
+	./configure "$0"
 else
 	./configure
 fi
