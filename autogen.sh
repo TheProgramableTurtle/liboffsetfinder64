7cd34d0c5cc4b7300657c280ee812f4fbc3e677c
Mika Jones <mlljones@icloud.com>
2020-07-16 12:54:21 +0100

    Fix build on Linux

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+if [ $# > 1 ];
+then
+	if [ $1 == "--no-mach-o" ];
+	then
+		echo "building without machopatchfinder, kernelpatchfinder"
+		mv ./makefiles/include-no-mach-o.make ./include/Makefile.am
+		mv ./makefiles/liboffsetfinder64-no-mach-o.make ./liboffsetfinder64/Makefile.am
+	else 
+		mv ./makefiles/include.make ./include/Makefile.am
+		mv ./makefiles/liboffsetfinder64.make ./liboffsetfinder64/Makefile.am
+	fi
+fi
+
 aclocal -I m4
 autoconf
 autoheader
